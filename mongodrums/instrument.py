dd059ea51d765c25e5629b0653fc79e3b9fc8028
Greg Banks <gbanks@mybasis.com>
2014-03-04 18:35:13 -0800

    fix some outstanding issues and bump version
    - handle __package__ correctly
    - only call explain after cursor has been completely initialized (after
      chained calls like `limit`)
    - remove professor dependency
    - update gevent requirement

--- mongodrums/instrument.py
+++ mongodrums/instrument.py
@@ -1,4 +1,3 @@
-import inspect
 import logging
 import random
 import socket
@@ -10,17 +9,20 @@ from abc import ABCMeta, abstractmethod
 from contextlib import contextmanager
 from functools import partial, update_wrapper
 from types import MethodType
+from weakref import WeakSet
 
 import pymongo
 
 from bson.json_util import dumps
 from bunch import Bunch
+from pymongo.cursor import Cursor 
 from pymongo.errors import OperationFailure
 
 from .config import (
     configure, get_config, register_update_callback, unregister_update_callback
 )
 from .pusher import push
+from .util import get_source
 
 
 class Wrapper(object):
@@ -46,23 +48,6 @@ class Wrapper(object):
     def __call__(self, *args, **kwargs):
         pass
 
-    def _get_pkg(globals_):
-        if globals_.get('__package__', None) is not None:
-            return globals_['__package__']
-        return globals_['__name__'].rpartition('.')[0]
-
-    def _get_source(self):
-        # cut the first two frames off the stack since the first is this frame
-        # and the second *should be* the wrapper's frame
-        frame = \
-            filter(lambda f: self._get_pkg(f[0].f_globals) \
-                             not in self._filter_packages,
-                   inspect.stack()[2:])[0]
-        try:
-            return '%s:%d' % (frame[1], frame[2])
-        finally:
-            del frame
-
     @classmethod
     @abstractmethod
     def wrap(cls):
@@ -83,42 +68,115 @@ class Wrapper(object):
             cls.unwrap()
 
 
-class FindWrapper(Wrapper):
+class _CursorMethodWrapper(Wrapper):
+    _ids = WeakSet()
+    _ids_lock = threading.RLock()
+    _method_name = None
+    
+    def __new__(cls, *args, **kwargs):
+        if cls._method_name is None:
+            raise RuntimeError('_method_name must be declared')
+        return super(_CursorMethodWrapper, cls).__new__(cls, *args, **kwargs)
+
+    @classmethod
+    def track_cursor(cls, cursor):
+        with cls._ids_lock:
+            cls._ids.add(cursor)
+
     def __call__(self, self_, *args, **kwargs):
-        curs = self._func(self_, *args, **kwargs)
-        if random.random() < self._frequency:
+        with self.__class__._ids_lock:
+            tracking = self_ in self.__class__._ids
+        if tracking:
+            with self.__class__._ids_lock:
+                assert(self_ in self.__class__._ids)
+                self.__class__._ids.discard(self_)
             try:
-                explain = curs.explain()
+                explain = self_.explain()
             except (TypeError, OperationFailure):
                 explain = {'error': traceback.format_exc()}
                 logging.exception('error trying to run explain on curs')
-            push({'type': 'explain',
-                  'function': 'find',
-                  'database': self_.database.name,
-                  'collection': self_.name,
-                  'query': dumps(args[0] if len(args) > 0 else {},
-                                 sort_keys=True),
-                  'explain': explain,
-                  'source': self._get_source()})
+            try:
+                push({'type': 'explain',
+                      'function': 'find',
+                      'database': self_.collection.database.name,
+                      'collection': self_.collection.name,
+                      'query': dumps(args[0] if len(args) > 0 else {},
+                                     sort_keys=True),
+                      'explain': explain,
+                      'source': get_source(self._filter_packages)})
+            except Exception:
+                logging.exception('exception pushing explain data for find')
+        return self._func(self_)
+
+    @classmethod
+    def wrap(cls):
+        with cls._lock:
+            meth = getattr(pymongo.cursor.Cursor, cls._method_name)
+            if not isinstance(meth, cls):
+                meth = cls(meth)
+                setattr(pymongo.cursor.Cursor, cls._method_name, meth)
+                register_update_callback(meth._configure)
+        return meth
+
+    @classmethod
+    def unwrap(cls):
+        with cls._lock:
+            meth = getattr(pymongo.cursor.Cursor, cls._method_name)
+            if isinstance(meth, cls):
+                unregister_update_callback(meth._configure)
+                setattr(pymongo.cursor.Cursor, cls._method_name, meth._func)
+
+
+class _CursorNextWrapper(_CursorMethodWrapper):
+    _method_name = 'next'
+
+
+class _CursorCountWrapper(_CursorMethodWrapper):
+    _method_name = 'count'
+
+
+class _CursorDistinctWrapper(_CursorMethodWrapper):
+    _method_name = 'distinct'
+
+
+_cursor_terminators = [_CursorNextWrapper, _CursorCountWrapper,
+                       _CursorDistinctWrapper]
+
+
+class FindWrapper(Wrapper):
+    def __init__(self, func):
+        super(FindWrapper, self).__init__(func)
+        self._cursor_wrappers = None
+    
+    def __call__(self, self_, *args, **kwargs):
+        curs = self._func(self_, *args, **kwargs)
+        if random.random() < self._frequency:
+            assert(self._cursor_wrappers is not None)
+            _CursorMethodWrapper.track_cursor(curs)
         return curs
 
     @classmethod
     def wrap(cls):
         with cls._lock:
             if not isinstance(pymongo.collection.Collection.find, cls):
-                pymongo.collection.Collection.find = \
-                    cls(pymongo.collection.Collection.find)
-                register_update_callback(
-                    pymongo.collection.Collection.find._configure)
+                instance = cls(pymongo.collection.Collection.find)
+                register_update_callback(instance._configure)
+                instance._cursor_wrappers = []
+                for cursor_wrapper in _cursor_terminators:
+                    instance._cursor_wrappers.append(cursor_wrapper.wrap())
+                pymongo.collection.Collection.find = instance
+        return pymongo.collection.Collection.find
 
     @classmethod
     def unwrap(cls):
         with cls._lock:
             if isinstance(pymongo.collection.Collection.find, cls):
-                unregister_update_callback(
-                    pymongo.collection.Collection.find._configure)
-                pymongo.collection.Collection.find = \
-                    pymongo.collection.Collection.find._func
+                instance = pymongo.collection.Collection.find
+                for cursor_wrapper in instance._cursor_wrappers:
+                    cursor_wrapper.unwrap()
+                instance._cursor_wrappers = None
+                unregister_update_callback(instance._configure)
+                pymongo.collection.Collection.find = instance._func
 
 
 class UpdateWrapper(Wrapper):
@@ -130,13 +188,16 @@ class UpdateWrapper(Wrapper):
             except (TypeError, OperationFailure):
                 explain = {'error': traceback.format_exc()}
                 logging.exception('error trying to run explain on curs')
-            push({'type': 'explain',
-                  'function': 'update',
-                  'database': self_.database.name,
-                  'collection': self_.name,
-                  'query': dumps(args[0], sort_keys=True),
-                  'explain': explain,
-                  'source': self._get_source()})
+            try:
+                push({'type': 'explain',
+                      'function': 'update',
+                      'database': self_.database.name,
+                      'collection': self_.name,
+                      'query': dumps(args[0], sort_keys=True),
+                      'explain': explain,
+                      'source': get_source(self._filter_packages)})
+            except Exception:
+                logging.exception('exception pushing explain data for update')
         return self._func(self_, *args, **kwargs)
 
     @classmethod
@@ -147,6 +208,7 @@ class UpdateWrapper(Wrapper):
                     cls(pymongo.collection.Collection.update)
                 register_update_callback(
                     pymongo.collection.Collection.update._configure)
+        return pymongo.collection.Collection.update
 
     @classmethod
     def unwrap(cls):
@@ -166,8 +228,8 @@ def start(config=None):
 
 
 def stop():
-    FindWrapper.unwrap()
     UpdateWrapper.unwrap()
+    FindWrapper.unwrap()
 
 
 @contextmanager
